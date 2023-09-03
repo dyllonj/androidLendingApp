5483f4bf7a3af5f653909dd046dc9c826a060b30
dyllonj <59544062+dyllonj@users.noreply.github.com>
2023-09-03 17:26:58 -0600

    Update main.dart
    formatting specific to tablet size and added button element

--- main.dart
+++ main.dart
@@ -24,54 +24,81 @@ class MyHomePage extends StatelessWidget {
       ),
       body: SingleChildScrollView(
         child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
+          padding: const EdgeInsets.all(32.0),
+          child: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+              
               // Banner
               Container(
-                padding: EdgeInsets.all(20),
+                padding: EdgeInsets.all(30),
                 color: Colors.blue,
                 child: Text(
                   'Welcome to [Platform Name]! Your path to financial understanding starts here.',
-                  style: TextStyle(fontSize: 20, color: Colors.white),
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 24, color: Colors.white),
                 ),
               ),
-              SizedBox(height: 20),
+              SizedBox(height: 40),
               
               // Login Section
-              TextField(
-                decoration: InputDecoration(
-                  labelText: 'Inmate ID',
-                  border: OutlineInputBorder(),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 300,
+                    padding: EdgeInsets.all(8.0),
+                    child: TextField(
+                      textAlign: TextAlign.center,
+                      decoration: InputDecoration(
+                        labelText: 'Inmate ID',
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 40),
+                  Container(
+                    width: 300,
+                    padding: EdgeInsets.all(8.0),
+                    child: TextField(
+                      textAlign: TextAlign.center,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        labelText: 'Password',
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 40),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.blue,
+                  padding: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
                 ),
+                child: Text('Login', style: TextStyle(fontSize: 18)),
+                onPressed: () {
+                  // Handle the login logic
+                },
               ),
               SizedBox(height: 20),
-              TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  labelText: 'Password',
-                  border: OutlineInputBorder(),
-                ),
+              TextButton(
+                child: Text('Forgot Password', style: TextStyle(fontSize: 16)),
+                onPressed: () {
+                  // Navigate to the help page
+                },
               ),
-              SizedBox(height: 10),
-              Align(
-                alignment: Alignment.centerRight,
-                child: TextButton(
-                  child: Text('Forgot Password'),
-                  onPressed: () {
-                    // Navigate to the help page
-                  },
-                ),
-              ),
-              SizedBox(height: 20),
+              SizedBox(height: 40),
 
               // Informational Section
               Text(
                 'Our Mission: [Brief summary of platform\'s mission, goals, and services.]',
-                style: TextStyle(fontSize: 18),
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 22),
               ),
-              SizedBox(height: 20),
+              SizedBox(height: 40),
               
               // Language Selector
               DropdownButton<String>(
@@ -83,17 +110,19 @@ class MyHomePage extends StatelessWidget {
                 onChanged: (String? value) {
                   // Handle language change logic
                 },
-                hint: Text('Select Language'),
+                hint: Text('Select Language', style: TextStyle(fontSize: 18)),
               ),
               
               // Footer
               Container(
-                padding: EdgeInsets.symmetric(vertical: 20),
+                padding: EdgeInsets.symmetric(vertical: 30),
                 child: Column(
                   children: [
-                    Text('Legal Disclaimers'),
-                    Text('Terms of Service'),
-                    Text('Privacy Policy'),
+                    Text('Legal Disclaimers', style: TextStyle(fontSize: 18)),
+                    SizedBox(height: 10),
+                    Text('Terms of Service', style: TextStyle(fontSize: 18)),
+                    SizedBox(height: 10),
+                    Text('Privacy Policy', style: TextStyle(fontSize: 18)),
                     // Add other links as needed
                   ],
                 ),
