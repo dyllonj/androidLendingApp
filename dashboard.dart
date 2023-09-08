21f37da367ac7db0e3147c38df2650a671cf8223
dyllonj <59544062+dyllonj@users.noreply.github.com>
2023-09-08 17:28:26 -0600

    Update dashboard.dart

--- dashboard.dart
+++ dashboard.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'loanapplicationpage.dart';
 import 'financialeducationpage.dart';
-import 'faq_page.dart';  // Ensure these are the correct paths
-import 'how_it_works_page.dart';  // Ensure these are the correct paths
+import 'support_ticket_page.dart';
 
 class DashboardPage extends StatelessWidget {
   @override
@@ -37,19 +36,17 @@ class DashboardPage extends StatelessWidget {
                 ),
               ),
               SizedBox(height: 20),
-
               Center(
                   child: Text('Total Interest Amount Due: [interestAmount]',
                       style: const TextStyle(fontSize: 20))),
               SizedBox(height: 20),
-
               Center(
                   child: Text('Total Amount Due: [totalAmount]',
                       style: const TextStyle(fontSize: 20))),
               SizedBox(height: 20),
-
               Container(
                 width: MediaQuery.of(context).size.width * 0.6,
+                height: 50,
                 child: ElevatedButton(
                   child: Text('Pay Loan with Commissary Account',
                       style: const TextStyle(fontSize: 18)),
@@ -59,15 +56,27 @@ class DashboardPage extends StatelessWidget {
                 ),
               ),
               SizedBox(height: 20),
-
               Center(
                   child: Text('Lending History',
                       style: const TextStyle(fontSize: 18))),
-              Center(child: LendingHistoryWidget()),  // Centered the widget
+              DataTable(
+                columns: [
+                  DataColumn(label: Text('Amount')),
+                  DataColumn(label: Text('Date')),
+                  DataColumn(label: Text('Purpose')),
+                ],
+                rows: [
+                  DataRow(cells: [
+                    DataCell(Text('\$50')),
+                    DataCell(Text('01/01/2023')),
+                    DataCell(Text('Loan'))
+                  ]),
+                ],
+              ),
               SizedBox(height: 20),
-
               Container(
                 width: MediaQuery.of(context).size.width * 0.6,
+                height: 50,
                 child: ElevatedButton(
                   child: Text('Lend Now', style: const TextStyle(fontSize: 18)),
                   onPressed: () {
@@ -79,9 +88,9 @@ class DashboardPage extends StatelessWidget {
                 ),
               ),
               SizedBox(height: 20),
-
               Container(
                 width: MediaQuery.of(context).size.width * 0.6,
+                height: 50,
                 child: ElevatedButton(
                   child: Text('Financial Education',
                       style: const TextStyle(fontSize: 18)),
@@ -94,34 +103,20 @@ class DashboardPage extends StatelessWidget {
                 ),
               ),
               SizedBox(height: 20),
-
               Container(
                 width: MediaQuery.of(context).size.width * 0.6,
+                height: 50,
                 child: ElevatedButton(
-                  child: Text('Frequently Asked Questions',
-                      style: const TextStyle(fontSize: 18)),
+                  child: Text('Contact Us', style: const TextStyle(fontSize: 18)),
                   onPressed: () {
                     Navigator.of(context).push(
-                      MaterialPageRoute(builder: (context) => FAQPage()),
+                      MaterialPageRoute(
+                          builder: (context) => SupportTicketPage()),
                     );
                   },
                 ),
               ),
               SizedBox(height: 20),
-
-              Container(
-                width: MediaQuery.of(context).size.width * 0.6,
-                child: ElevatedButton(
-                  child: Text('How It Works', style: const TextStyle(fontSize: 18)),
-                  onPressed: () {
-                    Navigator.of(context).push(
-                      MaterialPageRoute(builder: (context) => HowItWorksPage()),
-                    );
-                  },
-                ),
-              ),
-              SizedBox(height: 20),
-
               Container(
                 padding: EdgeInsets.all(20),
                 color: Colors.yellowAccent,
@@ -145,25 +140,3 @@ class DashboardPage extends StatelessWidget {
     );
   }
 }
-
-class LendingHistoryWidget extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: 200,
-      width: MediaQuery.of(context).size.width * 0.6,  
-      child: ListView.builder(
-        itemCount: 5, 
-        itemBuilder: (context, index) {
-          return Center(  
-            child: ListTile(
-              title: Text('\$[amount]', textAlign: TextAlign.center), 
-              subtitle: Text('Date: [date]', textAlign: TextAlign.center), 
-            ),
-          );
-        },
-      ),
-    );
-  }
-}
-
